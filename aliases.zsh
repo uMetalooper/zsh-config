2e83d13c69ba30e738e48079a818caf2d49efdf0
ducthan <dthan@uber.com>
2024-08-05 13:08:32 +0200

    Re-organize

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,11 @@
+# Description: Aliases for common commands
+
+# Arcanist
 alias adna="arc diff --noautoland "
 alias asd="arc sync --dependencies"
 alias as="arc sync"
 
-alias gbr="git-bzl refresh"
-
 alias needsreview='arc list | grep -oE '\''D.+'\'' | awk -F '\'': '\'' '\''{print "Needs Review *["$2"](http:\/\/code.uberinternal.com\/"$1")*"}'\'''
+
+# Git+Bazel
+alias gbr="git-bzl refresh"
